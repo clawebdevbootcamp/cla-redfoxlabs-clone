ea9202edb9f03a1a9e5d5b6a18310b7eb7855e1e
RayaneA7 <ka_kebir@esi.dz>
2022-04-30 20:41:56 +0100

    added some js for the nav bar dispal and the nav bar color toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,29 @@
 let links = document.querySelector('.links')
-let links = document.querySelector('nav button')
+let nav = document.querySelector('nav > div')
+let button = document.querySelector('.nav')
 
-function ToggleNavBar () {}
+function ToggleNavBar () {
+  if (links.classList.contains('linksNotHidden')) {
+    links.classList.remove('linksNotHidden')
+    links.classList.add('linksHidden')
+  } else {
+    links.classList.add('linksNotHidden')
+    links.classList.remove('linksHidden')
+  }
+}
+
+function backgroundColorChng () {
+  if (scrollY) {
+    nav.style.backgroundColor = '#000000'
+  } else {
+    nav.style.backgroundColor = ''
+  }
+}
+
+button.addEventListener('click', () => {
+  ToggleNavBar()
+})
+
+document.addEventListener('scroll', () => {
+  backgroundColorChng()
+})
